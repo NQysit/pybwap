d808343cc890ed15d4c86c0b1351b6f43036579a
Marcos Diaz <marcos.diaz.cast@gmail.com>
2015-10-04 10:50:04 +0200

    Put serve function in app __init__.py

--- pybwap/__init__.py
+++ pybwap/__init__.py
@@ -1,6 +1,8 @@
 
 # -*- coding: utf-8 -*-
 
+import os
+
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
@@ -15,3 +17,11 @@ app.register_blueprint(ch_0x00_blueprint)
 
 from .xss_0x01 import xss_0x01_blueprint
 app.register_blueprint(xss_0x01_blueprint)
+
+@app.route('/static/<folder>/<name>')
+def serve(folder, name):
+    '''
+    Only for developing purpose
+    '''
+    dirname = os.path.dirname(__file__)
+    return send_from_directory(os.path.join(dirname, 'static', folder), filename=name)
--- pybwap/main/views.py
+++ pybwap/main/views.py
@@ -1,21 +1,12 @@
 
 # -*- coding: utf-8 -*-
 
-import os
 
-import sys
-
-from flask import render_template
+from flask import render_template, send_from_directory
 from . import main_blueprint
 
 @main_blueprint.route('/')
 def index():
     return render_template('index.html')
 
-@main_blueprint.route('/static/<folder>/<name>')
-def serve(folder, name):
-    '''
-    Only for developing purpose
-    '''
-    dirname = os.path.dirname(__file__)
-    return send_from_directory(os.path.join(dirname, 'static', folder), filename=name)
+
